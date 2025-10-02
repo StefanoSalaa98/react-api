dd3bc9cba30c5ddcc0a8b3073d6ed561e5575d79
StefanoSalaa98 <sala.stefano98@gmail.com>
2025-10-02 17:11:17 +0200

    Bonus 1: Stampare sia una lista delle attrici che degli attori, separatamente.

--- src/components/MyMain.jsx
+++ src/components/MyMain.jsx
@@ -3,7 +3,10 @@ import MyActress from "./MyActress";
 
 export default function MyMain() {
 
+    //variabile di stato per le attrici
     const [actresses, setActresses] = useState([]);
+    //variabile di stato per gli attori
+    const [actors, setActors] = useState([]);
 
 
     function fetchActresses() {
@@ -14,22 +17,40 @@ export default function MyMain() {
             )
     }
 
+    function fetchActors() {
+        axios.get("https://lanciweb.github.io/demo/api/actors/")
+            .then((res) => setActors(res.data))
+            .catch(error => console.log(error))
+    }
+
     //useEffect senza dipendenze viene richiamato solo la prima volta al caricamento della pagina
 
     useEffect(() => {
         fetchActresses()
+        fetchActors()
     }, [])
 
 
     return (
-        <div className="container">
-            {actresses.map(actress => (
-                <div key={actress.id} className="card">
-                    <MyActress
-                        actress={actress}
-                    />
-                </div>
-            ))}
-        </div>
+        <>
+            <div className="container">
+                {actresses.map(actress => (
+                    <div key={actress.id} className="card woman">
+                        <MyActress
+                            actress={actress}
+                        />
+                    </div>
+                ))}
+            </div>
+            <div className="container">
+                {actors.map(actress => (
+                    <div key={actress.id} className="card man">
+                        <MyActress
+                            actress={actress}
+                        />
+                    </div>
+                ))}
+            </div>
+        </>
     )
 }
